f222ca307d9966bce806d9046f96053a63686994
Taylor Najjar <16545112+taylteller@users.noreply.github.com>
2019-07-02 11:17:07 -0400

    start tests

---
+++ test/script.test/resetIndex.spec.js
@@ -0,0 +1,39 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+
+// const index = require('../index.js');
+const resetIndex = require('../../script/resetIndex.js');
+
+describe.skip('Main call', () => {
+  it('should call resetIndex', (done) => {
+    const mySpy = sinon.spy(resetIndex);
+    mySpy();
+    expect(mySpy.called).to.be.true;
+    done();
+  })
+});
+
+describe('ResetIndex', function () {
+  // it('should return a statusCode', async function () {
+  //   this.timeout(15000);
+  //   const response = await resetIndex();
+  //   expect(response.statusCode).to.be.a('number');
+  // });
+  //
+  // it('statusCode should be 200', function () {
+  //   this.timeout(15000);
+  //   return resetIndex().then(function (resp) {
+  //     expect(resp.statusCode).to.equal(200);
+  //   });
+  // });
+
+  it('should return statusCode as a number', function () {
+    this.timeout(60000);
+    return resetIndex().then(function (resp) {
+      expect(resp.statusCode).to.be.a('number');
+    });
+  })
+});
+
+
