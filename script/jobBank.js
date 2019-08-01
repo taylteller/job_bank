0272897dc882e050bcfe76b20e7e78f0fd70727b
Taylor Najjar <16545112+taylteller@users.noreply.github.com>
2019-08-01 11:49:51 -0400

    add tests for jobBank.js and refactor jobBank to resolve scope issues and get tests working

--- script/jobBank.js
+++ script/jobBank.js
@@ -6,75 +6,87 @@ const english = 'en/';
 const french = 'fr/';
 const mainEndpoint = 'on';
 
-_endpointCall = (url, count=0) => {
-  return axios.get(url, {
-    headers: {
-      Cookie: "[insert correct cookie]"
-    },
-    responseType: 'text'
-  }).then(resp => {
-    return resp;
-  }).catch(err => {
-    if (count < 5) {
-      count++;
-      return _endpointCall(url, count);
-    }
-    throw err;
-  });
-};
+const jobBank = {
 
-const _toJson = function (xml) {
-  // console.log('xml', xml);
-  return new Promise((resolve, reject) => {
-    parseString(xml.data, (err, result) => {
-      if(err) {
-        // console.log('err', err)
-        reject(err);
+  _endpointCall: function(url, count=0) {
+    return axios.get(url, {
+      headers: {
+        Cookie: "[insert correct cookie]"
+      },
+      responseType: 'text'
+    }).then(resp => {
+      return resp;
+    }).catch(err => {
+      if (count < 5) {
+        count++;
+        return _endpointCall(url, count);
       }
-      // The initial endpoint call will return an array of objects, in which case we want the whole array
-      // An individual job record will return a single object array, in which case we want the object itself
-      if (result.SolrResponse.Documents[0].Document.length === 1) {
-        resolve(result.SolrResponse.Documents[0].Document[0]);
-      }
-      resolve(result.SolrResponse.Documents[0].Document);
-    })
-  })
-};
-
-let lang = 'en/';
-
-//TODO: does every nested then need its own error catch?
-module.exports.createJobsArray = async (initialDatasetJSON, lang) => {
-  lang = lang.toLowerCase() === 'fr' || lang.toLowerCase() === 'french' ? french : english;
-
- return await Promise.all(initialDatasetJSON.map(async x => {
-    let jobUrl = baseUrl + lang + x.jobs_id[0] + '.xml';
-    return await _endpointCall(jobUrl).then(resp => {
-      return _toJson(resp).then(resp =>{
-        return simplify(resp);
-      }, err => {
-        throw err;
-      });
+      throw err;
     });
-  }));
+  },
+
+  _toJson: function (xml) {
+    return new Promise((resolve, reject) => {
+      parseString(xml.data, (err, result) => {
+        if(err) {
+          reject(err);
+        }
+        // The initial endpoint call will return an array of objects, in which case we want the whole array
+        // An individual job record will return a single object array, in which case we want the object itself
+        if (result.SolrResponse.Documents[0].Document.length === 1) {
+          resolve(result.SolrResponse.Documents[0].Document[0]);
+        }
+        resolve(result.SolrResponse.Documents[0].Document);
+      })
+    })
+  },
+
+  getInitialIDs: function() {
+    // return jobBank._endpointCall(baseUrl + english + mainEndpoint);
+    return jobBank._endpointCall(baseUrl + english + mainEndpoint).then(resp => {
+      return jobBank._toJson(resp);
+    });
+  },
+
+  createJobsArray: async (initialDatasetJSON, lang) => {
+    lang = lang.toLowerCase() === 'fr' || lang.toLowerCase() === 'french' ? french : english;
+
+    const jobs = await Promise.all(initialDatasetJSON.map(async x => {
+      let jobUrl = baseUrl + lang + x.jobs_id[0] + '.xml';
+      return await jobBank._endpointCall(jobUrl).then(resp => {
+        return jobBank._toJson(resp).then(resp =>{
+          return simplify(resp);
+        });
+      }).catch(error => {
+        console.log('Cannot create job ID ' + x.jobs_id[0]);
+        error.jobId = x.jobs_id[0];
+        return error;
+      });
+    }));
+
+    let errors = [];
+    const validJobs = jobs.filter((job) => {
+      if (job instanceof Error) {
+        errors.push(job.jobId);
+        return false
+      }
+      return true;
+    });
+
+    return {
+      jobs: validJobs,
+      errors,
+    }
+  },
+
+  createBulkPushArray: (arrayOfJobs, index) => {
+    return arrayOfJobs.reduce((accumulator, currentValue) => {
+      accumulator.push({index: { _index: index}});
+      accumulator.push(currentValue);
+      return accumulator;
+    }, []);
+  }
+
 };
 
-//TODO: does every nested then need its own error catch?
-module.exports.getInitialIDs = async () => {
-  return await _endpointCall(baseUrl + english + mainEndpoint).then(resp => {
-    return _toJson(resp);
-  }, err => {
-    throw err;
-  });
-};
-
-module.exports.createBulkPushArray = (arrayOfJobs, index) => {
-  return arrayOfJobs.reduce((accumulator, currentvalue) => {
-    accumulator.push({index: { _index: index}});
-    accumulator.push(currentvalue);
-    return accumulator;
-  }, []);
-};
-
-module.exports._endpointCall = _endpointCall;
-module.exports._toJson = _toJson;
+module.exports = jobBank;
---
+++ test/script/jobBank.spec.js
@@ -0,0 +1,336 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+const sinon = require('sinon');
+
+const axios = require('axios');
+const jobBank = require('../../script/jobBank.js');
+
+describe('_endpointCall', () => {
+  describe('when endpoint is successfully reached', () => {
+
+    before(() => {
+      sinon.stub(axios, 'get').resolves({ status: 200, statusText: 'ok', data: '' });
+    });
+
+    after(() => {
+      axios.get.restore();
+    });
+
+    it('should return a 200 status code', () => {
+      return jobBank._endpointCall('www.test.noturl').then(result => {
+        return expect(result.status).to.equal(200);
+      });
+    });
+  });
+
+  describe('when endpoint first fails but succeeds on a subsequent try', () => {
+
+    before(() => {
+      sinon.stub(axios, 'get')
+        .onFirstCall().rejects()
+        .onSecondCall().resolves({ status: 200, statusText: 'ok', data: '' });
+    });
+
+    after(() => {
+      axios.get.restore();
+    });
+
+    it('should return a 200 status code', () => {
+      return jobBank._endpointCall('www.test.noturl').then(result => {
+        return expect(result.status).to.equal(200);
+      }, error => {console.log(error)});
+    });
+  });
+
+  describe('when endpoint is never reached', () => {
+
+    before(() => {
+      sinon.stub(axios, 'get')
+        .rejects({err: 'no dice'});
+    });
+
+    after(() => {
+      axios.get.restore();
+    });
+
+    it('should throw an error', () => {
+      return jobBank._endpointCall('stillnotareal.url').then(result => {
+        return expect(result).to.equal('this should not be hit; you want to hit the error')
+      }, err => {
+        return expect(err).to.deep.equal({err: 'no dice'});
+      })
+    });
+  });
+});
+
+describe('_toJson', () => {
+  it('doesn\'t need to be tested, as it is only a wrapper around a library', () => {
+  });
+});
+
+describe('getInitialIDs', () => {
+
+  let endpointResponse = {data: '<SolrResponse>\n<Header>\n<status>0</status>\n<QTime>0</QTime>\n' +
+      '<numFound>1</numFound>\n</Header>\n<Documents>\n' +
+      '<Document>\n<jobs_id>30514572</jobs_id>\n' +
+      '<file_update_date>2019-06-14T08:38:00Z</file_update_date>\n</Document>\n' +
+      '<Document>\n<jobs_id>30514467</jobs_id>\n' +
+      '<file_update_date>2019-06-14T08:37:00Z</file_update_date>\n</Document>\n' +
+      '<Document>\n<jobs_id>30514479</jobs_id>\n' +
+      '<file_update_date>2019-06-14T08:37:00Z</file_update_date>\n</Document>\n' +
+      '</Documents>\n</SolrResponse>\n'};
+
+  describe('if endpoint is successfully reached and xml is successfully parsed', () => {
+
+    before(() => {
+      sinon.stub(jobBank, '_endpointCall').resolves(endpointResponse);
+    });
+
+    after(() => {
+      jobBank._endpointCall.restore();
+    });
+
+    it('should return an array of objects', () => {
+      return jobBank.getInitialIDs().then( result => {
+        return expect(result).to.deep.equal([
+          {
+            jobs_id: [ '30514572' ],
+            file_update_date: [ '2019-06-14T08:38:00Z' ]
+          },
+          {
+            jobs_id: [ '30514467' ],
+            file_update_date: [ '2019-06-14T08:37:00Z' ]
+          },
+          {
+            jobs_id: [ '30514479' ],
+            file_update_date: [ '2019-06-14T08:37:00Z' ]
+          }
+        ]);
+      });
+    });
+  });
+
+  describe('if endpoint fails', () => {
+
+    before(() => {
+      sinon.stub(jobBank, '_endpointCall').rejects(new Error('testError'));
+    });
+
+    after(() => {
+      jobBank._endpointCall.restore();
+    });
+
+    it('should throw an error', async () => {
+      const func = () => jobBank.getInitialIDs();
+      return expect(func()).to.be.rejectedWith(Error, 'testError');
+    });
+  });
+
+  describe('if endpoint is successfully reached but xml fails to parse', () => {
+
+    before(() => {
+      sinon.stub(jobBank, '_endpointCall').resolves({ohNo: 'we are missing the expected data key! gasp!'});
+      sinon.stub(jobBank, '_toJson').rejects(new Error('testError'));
+    });
+
+    after(() => {
+      jobBank._endpointCall.restore();
+      jobBank._toJson.restore();
+    });
+
+    it('should throw an error', () => {
+      const func = () => jobBank.getInitialIDs();
+      return expect(func()).to.be.rejectedWith(Error, 'testError');
+    });
+  });
+});
+
+describe('createJobsArray', () => {
+
+  let initialDatasetJSON = [
+    {
+      jobs_id: ['30954482'],
+      file_update_date: ['2019-08-01T09:59:00Z']
+    },
+    {
+      jobs_id: ['30954475'],
+      file_update_date: ['2019-08-01T09:58:00Z']
+    }
+  ];
+
+  let endpointResponse = '<SolrResponse>\n<Header>\n<status>0</status>\n<QTime>0</QTime>\n' +
+    '<numFound>1</numFound>\n</Header>\n<Documents>\n<Document>\n' +
+    '<jobs_id>30954481</jobs_id>\n<noc_2011>6622</noc_2011>\n' +
+    '<skill_level>D</skill_level>\n<remote_cd>1275007</remote_cd>\n<title>produce ' +
+    'clerk, supermarket</title>\n<noc>6622</noc>\n' +
+    '<province_cd>\n<string>ON</string>\n</province_cd>\n<salary>\n<string>$14.00 ' +
+    'hourly</string>\n</salary>\n<employer_name>\n<string>Farm Boy</string>\n' +
+    '</employer_name>\n</Document>\n</Documents>\n</SolrResponse>\n';
+
+  let _toJsonResponse = {
+    jobs_id: ['30954478'],
+    noc_2011: ['6611'],
+    skill_level: ['D'],
+    remote_cd: ['1275008'],
+    title: ['cashier, supermarket'],
+    noc: ['6611'],
+    naics_id: ['26'],
+    hours: ['1 to 19 hours per week'],
+    employer_name_case: ['Farm Boy'],
+    employer_name_string: ['Farm Boy'],
+    num_positions: ['1'],
+    job_senior_flag: ['false']
+  };
+
+
+  describe('when all jobs successful', () => {
+
+    before(() => {
+      sinon.stub(jobBank, '_endpointCall').resolves(endpointResponse);
+      sinon.stub(jobBank, '_toJson').resolves(_toJsonResponse);
+    });
+
+    after(() => {
+      jobBank._endpointCall.restore();
+      jobBank._toJson.restore();
+    });
+
+    it('should create an english dataset', () => {
+      const func = () => jobBank.createJobsArray(initialDatasetJSON, 'en');
+      return expect(func()).to.eventually.deep.equal({
+        jobs: [
+          {
+            jobs_id: '30954478',
+            noc_2011: '6611',
+            skill_level: 'D',
+            remote_cd: '1275008',
+            title: 'cashier, supermarket',
+            noc: '6611',
+            naics_id: '26',
+            hours: '1 to 19 hours per week',
+            employer_name_case: 'Farm Boy',
+            employer_name_string: 'Farm Boy',
+            num_positions: '1',
+            job_senior_flag: 'false'
+          },
+          {
+            jobs_id: '30954478',
+            noc_2011: '6611',
+            skill_level: 'D',
+            remote_cd: '1275008',
+            title: 'cashier, supermarket',
+            noc: '6611',
+            naics_id: '26',
+            hours: '1 to 19 hours per week',
+            employer_name_case: 'Farm Boy',
+            employer_name_string: 'Farm Boy',
+            num_positions: '1',
+            job_senior_flag: 'false'
+          }
+        ],
+        errors: []
+      });
+    })
+  });
+
+  describe('when not all jobs successful', () => {
+
+    before(() => {
+      sinon.stub(jobBank, '_endpointCall')
+        .onFirstCall().resolves(endpointResponse)
+        .onSecondCall().rejects();
+      sinon.stub(jobBank, '_toJson').resolves(_toJsonResponse);
+    });
+
+    after(() => {
+      jobBank._endpointCall.restore();
+      jobBank._toJson.restore();
+    });
+
+    it('should return an object with a successful jobs array and an errors array', () => {
+      const func = () => jobBank.createJobsArray(initialDatasetJSON, 'en');
+      return expect(func()).to.eventually.deep.equal({
+        jobs: [
+          {
+            jobs_id: '30954478',
+            noc_2011: '6611',
+            skill_level: 'D',
+            remote_cd: '1275008',
+            title: 'cashier, supermarket',
+            noc: '6611',
+            naics_id: '26',
+            hours: '1 to 19 hours per week',
+            employer_name_case: 'Farm Boy',
+            employer_name_string: 'Farm Boy',
+            num_positions: '1',
+            job_senior_flag: 'false'
+          }
+        ],
+        errors: [ '30954475' ]
+      });
+    });
+  });
+
+  describe('createBulkPushArray', () => {
+
+    let arrayOfJobs = [
+      {
+        jobs_id: '30955317',
+        noc_2011: '8432',
+        skill_level: 'C',
+        remote_cd: '1273956',
+        title: 'greenhouse worker',
+        noc: '8432',
+        province_cd: 'ON',
+        salary: '$14.00 hourly',
+        employer_name: 'Pomas Farms Inc.'
+      },
+      {
+        jobs_id: '30955317',
+        noc_2011: '8432',
+        skill_level: 'C',
+        remote_cd: '1273956',
+        title: 'greenhouse worker',
+        noc: '8432',
+        province_cd: 'ON',
+        salary: '$14.00 hourly',
+        employer_name: 'Pomas Farms Inc.'
+      }
+    ];
+
+    describe('when it receives an array of jobs', () => {
+      it('should return an array of objects alternating between an es header object and a job object', () => {
+        const func = () => jobBank.createBulkPushArray(arrayOfJobs, 'job-bank-en');
+        return expect(func()).to.deep.equal([
+          { index: { _index: 'job-bank-en' } },
+          {
+            jobs_id: '30955317',
+            noc_2011: '8432',
+            skill_level: 'C',
+            remote_cd: '1273956',
+            title: 'greenhouse worker',
+            noc: '8432',
+            province_cd: 'ON',
+            salary: '$14.00 hourly',
+            employer_name: 'Pomas Farms Inc.'
+          },
+          { index: { _index: 'job-bank-en' } },
+          {
+            jobs_id: '30955317',
+            noc_2011: '8432',
+            skill_level: 'C',
+            remote_cd: '1273956',
+            title: 'greenhouse worker',
+            noc: '8432',
+            province_cd: 'ON',
+            salary: '$14.00 hourly',
+            employer_name: 'Pomas Farms Inc.'
+          }
+        ])
+      });
+    });
+  });
+
+});
